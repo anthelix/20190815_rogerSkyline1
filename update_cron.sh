f8bb1a882fa7bb37197fc08fd335ba247a4e59fb
anthelix <sdulou@free.fr>
2019-08-22 13:02:13 +0200

    update_cron

--- update_cron.sh
+++ update_cron.sh
@@ -1,45 +1,20 @@
 #!/bin/sh
 
-#----------------
-# VARIABLES
-#----------------
 OUTPUT="/var/log/update_script.log"
-TIME="Info.$(date +'%d-%m-%Y')"
-#----------------
-
-# FUNCTION
-#----------------
+TIME="Info du $(date +'%d-%m-%Y %H:%M:%S')"
 
 print_title()
 {
     echo "">>$OUTPUT
     echo "------------------------------------------" >>$OUTPUT
-    echo "\033[35;1m   $TIME\033[0m" >> $OUTPUT
+    echo "\033[35;1m   $TIME\033[0m" >>$OUTPUT
     echo "------------------------------------------" >>$OUTPUT
 }
 
-check_root()
-{
-    local meid=$(id -u)
-    if [ $meid -ne 0 ]
-    then
-    echo "Be root to use the script"
-    exit 999
-    fi
-}
 check_update()
 {
-	apt-get -y update && apt-get -y upgrade >>  $OUTPUT
-	
+	apt-get -y update 2>&1 && apt-get -y upgrade 2>&1 >>$OUTPUT
 }
 
-
-check_root
 print_title
 check_update
-
-
-#Réalisez un script qui met à jour l’ensemble des sources de package
-#puis de vos packages et 
-#qui log l’ensemble dans un fichier nommé /var/log/update_script.log.
-# Créez une tache planifiée pour ce script une fois par semaine à 4h00 du matin et à chaque reboot de la machine.
