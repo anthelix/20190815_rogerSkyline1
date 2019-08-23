aa18643eabd7d3c312b0a5eeb843e6d64ac830f7
anthelix <sdulou@free.fr>
2019-08-23 11:16:10 +0200

    fin de crontab

--- crontab_scrutiny.sh
+++ crontab_scrutiny.sh
@@ -1,45 +1,38 @@
 #!/bin/sh
 
-#Réalisez un script qui permet de surveiller les modifications du fichier /etc/crontab et 
-#envoie un mail à root si celui-ci a été modifié. 
-#Créez une tache plannifiée pour script tous les jours à minuit.
-
 # Nom du fichier a surveiller
-FILE=/etc/crontab1
+FILE=/etc/crontab
 # Date de la dernière modification du fichier ci-dessus
 DERNIERE_MODIF=$(date -r ${FILE} '+%d/%m/%Y')
 # Date de la veille
 DATE_BKP=$(date --date='yesterday' '+%d/%m/%Y')
 # Date du jour
 DATE=$(date '+%d/%m/%Y')
-
-# fichier du MD5 du fichier du jour
-md5sum $FILE > /etc/CronDailyMd5
+# Fichier du Sha256sum du fichier du jour
+sha256sum $FILE > /etc/CronDailySha
 # Fichier du backup
 FILE_BKP=/etc/crontab_bkp
-echo "1"
+# Poid du ficchier a surveiller
+WC_FILE=$(stat --format "%s" /etc/crontab1)
 
-if [ ! -f $FILE_BKP ]
+if [ ! -f $FILE_BKP ] #un fichier n’existe  pas 
 then
 	cp $FILE $FILE_BKP
 	md5sum $FILE_BKP > CronBkpMd5
-	echo "2"
 	exit
-elif [ CronDailyMd5 != CronBkpMd5 ]
-then
-	diff $FILE $FILE_BKP | egrep "<|>" > /etc/crontab.tmp
-	mail -s "Alerte niveau 3,  modifications de $FILE, consulter le fichier /etc/crontab.tmp" root
-	echo "3"
-elif [ DERNIERE_MODIF==DATE_BKP || DERNIERE_MODIF==DATE ]
-then
-	mail -s "Alerte	niveau 2, modification de la date de $FILE depuis hier" root
-	echo "4"
-elif [ wc -c $FILE != wc -c $FILE_BKP ] 
-then
-	mail -s -c root "Arlete niveau 1, modification de la taille de $FILE depuis hier" root
-	echo "5"
+else
+	WC_BKP=$(stat --format "%s" /etc/crontab_bkp)
+	if [ CronDailySha = CronBkpSha ]
+	then
+		diff $FILE $FILE_BKP | egrep "<|>" > /etc/crontab.tmp
+		cat /etc/crontab | mail -s "Alerte niveau 3,  modifications de $FILE, consulter le fichier /etc/crontab.tmp" root
+	elif [ $DERNIERE_MODIF = $DATE_BKP ] || [ $DERNIERE_MODIF = $DATE ]
+	then
+		cat /etc/crontab | mail -s "Alerte niveau 2, modification de la date de $FILE depuis hier" root
+	elif [ $WC_FILE -ne $WC_BKP ] 
+	then
+		cat /etc/crontab mail | -s "Arlete niveau 1, modification de la taille de $FILE depuis hier" root
+	fi
 fi
-
 cp $FILE $FILE_BKP
-md5sum $FILE_BKP > /etc/CronBkpMd5
-echo "6"
+sha256sum $FILE_BKP > /etc/CronBkpSha
